770467f0cbb10db4a2af87d674e55e38bc7b621e
Jonty <ljt20030312@Outlook.com>
2023-10-06 16:08:34 +0800

    ✨ feat(led.c, led.h): 新增了颜色选择对应的led

---
+++ EECM_programme/HARDWARE/led/led.c
@@ -0,0 +1,22 @@
+#include "led.h"
+
+void led_init(void);
+void set_red_led(uint8_t status);
+void set_blue_led(uint8_t status);
+void set_green_led(uint8_t status);
+
+void led_init(void)
+{
+}
+
+void set_red_led(uint8_t status)
+{
+}
+
+void set_blue_led(uint8_t status)
+{
+}
+
+void set_green_led(uint8_t status)
+{
+}
