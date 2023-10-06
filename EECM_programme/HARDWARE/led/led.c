691e5c45baaf19e9deec7ec3d591250d9035d2b6
Jonty <ljt20030312@Outlook.com>
2023-10-06 16:31:47 +0800

    ✨ feat: 增加通过led显示颜色选择结果

--- EECM_programme/HARDWARE/led/led.c
+++ EECM_programme/HARDWARE/led/led.c
@@ -1,3 +1,9 @@
+/***
+ * @Author: Jonty ljt20030312@Outlook.com
+ * @Date: 2023-10-06 16:19
+ * @LastEditTime: 2023-10-06 16:23
+ * @Description: led显示颜色选择结果
+ */
 #include "led.h"
 
 void led_init(void);
@@ -7,16 +13,54 @@ void set_green_led(uint8_t status);
 
 void led_init(void)
 {
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+    GPIO_InitTypeDef colour_led_initStructure;
+    colour_led_initStructure.GPIO_Mode = GPIO_Mode_OUT;
+    colour_led_initStructure.GPIO_OType = GPIO_OType_PP;
+    colour_led_initStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_1 | GPIO_Pin_0;
+    colour_led_initStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    colour_led_initStructure.GPIO_Speed = GPIO_Medium_Speed;
+
+    GPIO_Init(GPIOB, &colour_led_initStructure);
+
+    set_red_led(0);
+    set_blue_led(0);
+    set_green_led(0);
 }
 
 void set_red_led(uint8_t status)
 {
+    if (status == 1)
+    {
+        GPIO_SetBits(GPIOB, GPIO_Pin_0);
+    }
+    else
+    {
+        GPIO_ResetBits(GPIOB, GPIO_Pin_0);
+    }
 }
 
 void set_blue_led(uint8_t status)
 {
+    if (status == 1)
+    {
+        GPIO_SetBits(GPIOB, GPIO_Pin_1);
+    }
+    else
+    {
+        GPIO_ResetBits(GPIOB, GPIO_Pin_1);
+    }
 }
 
 void set_green_led(uint8_t status)
 {
+    if (status == 1)
+    {
+        GPIO_SetBits(GPIOB, GPIO_Pin_2);
+    }
+    else
+    {
+        GPIO_ResetBits(GPIOB, GPIO_Pin_2);
+    }
 }
