8005013f8d6d9d49a086ed98546a7ed59a43c331
Jonty <ljt20030312@Outlook.com>
2023-10-24 18:27:17 +0800

    🐞 fix(UART2.c): TX由PA2 -> PD5

--- EECM_programme/SYSTEM/usart2/UART2.c
+++ EECM_programme/SYSTEM/usart2/UART2.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
-#include "stm32f4xx.h"                  // Device header
+#include "stm32f4xx.h" // Device header
 #include "wit_c_sdk.h"
 
 void Usart2_init(unsigned int uiBaud)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	USART_InitTypeDef USART_InitStructure;
-	NVIC_InitTypeDef NVIC_InitStructure; 
+	NVIC_InitTypeDef NVIC_InitStructure;
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-	
+
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);    
-	
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource5, GPIO_AF_USART2);
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
-	 
+
 	USART_InitStructure.USART_BaudRate = uiBaud;
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
 	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_Parity = USART_Parity_No ;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-	USART_Init(USART2, &USART_InitStructure); 
-	
+	USART_Init(USART2, &USART_InitStructure);
+
 	USART_Cmd(USART2, ENABLE);
 	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
-	
+
 	USART_Cmd(USART2, ENABLE);
 	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
@@ -42,7 +45,7 @@ void Usart2_init(unsigned int uiBaud)
 void USART2_IRQHandler(void)
 {
 	unsigned char ucTemp;
-	if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
+	if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
 	{
 		ucTemp = USART_ReceiveData(USART2);
 		WitSerialDataIn(ucTemp);
@@ -50,13 +53,14 @@ void USART2_IRQHandler(void)
 	}
 }
 void Uart2_send(unsigned char *p_data, unsigned int uiSize)
-{	
+{
 	unsigned int i;
-	for(i = 0; i < uiSize; i++)
+	for (i = 0; i < uiSize; i++)
 	{
-		while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
-		USART_SendData(USART2, *p_data++);		
+		while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET)
+			;
+		USART_SendData(USART2, *p_data++);
 	}
-	while(USART_GetFlagStatus(USART2, USART_FLAG_TC) == RESET);
+	while (USART_GetFlagStatus(USART2, USART_FLAG_TC) == RESET)
+		;
 }
-
