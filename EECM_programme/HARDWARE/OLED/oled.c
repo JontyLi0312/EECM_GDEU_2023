fe2ff7ae1dd0e2e9d77c259d9ec92b2d06fbe3a9
Jonty <ljt20030312@Outlook.com>
2023-10-24 18:34:16 +0800

    🐞 fix(oled.c, oled.h): SDA由PD5 -> PG11

--- EECM_programme/HARDWARE/OLED/oled.c
+++ EECM_programme/HARDWARE/OLED/oled.c
@@ -744,19 +744,12 @@ void OLED_Init(void)
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD | RCC_AHB1Periph_GPIOG, ENABLE); // 使能PORTA~E,PORTG时钟
 
 	// GPIO初始化设置
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;	   // 普通输出模式
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;	   // 推挽输出
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz; // 100MHz
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;	   // 上拉
-	GPIO_Init(GPIOD, &GPIO_InitStructure);			   // 初始化
-
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP; // 推挽输出
 	GPIO_Init(GPIOD, &GPIO_InitStructure);		   // 初始化
 
 	// GPIO初始化设置
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_11;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;	   // 普通输出模式
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;	   // 推挽输出
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz; // 100MHz
