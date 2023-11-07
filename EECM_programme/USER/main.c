28170d3c8b57310b8f6841a5e9cc633432485015
Jonty <ljt20030312@Outlook.com>
2023-11-07 20:42:49 +0800

    ✨ feat(main.c): show max pitch value

--- EECM_programme/USER/main.c
+++ EECM_programme/USER/main.c
@@ -195,9 +195,12 @@ void TIM6_DAC_IRQHandler(void)
 
         jy901s_getData(&g_angleDatas);
         float pitch_max = 0;
-        if(g_angleDatas.pitch >= pitch_max){
+        if (g_angleDatas.pitch >= pitch_max)
+        {
             pitch_max = g_angleDatas.pitch;
         }
+        OLED_ShowNum(0, 30, (int32_t)pitch_max, 4, 8, 1);
+        OLED_Refresh();
 
         TIM_ClearITPendingBit(TIM6, TIM_IT_Update); // 清除中断标志位
     }
