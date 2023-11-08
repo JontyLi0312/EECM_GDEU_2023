b46820bfe94c1b37bd864009cef04a1afc4c7895
coolersf <3328151400@qq.com>
2023-11-08 23:24:19 +0800

    ✨ feat(main.c): 增加delay时间在舵机复位之后

--- EECM_programme/USER/main.c
+++ EECM_programme/USER/main.c
@@ -77,6 +77,7 @@ int main(void)
             Servo_Action();
             delay_ms(1500);
             Servo_Reset();
+            delay_ms(1000);
             g_flag = 2;
         }
         if (direction == 'L')
