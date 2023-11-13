e9ebba81e5bd21ddb229adc47f93dec31c7e976b
coolersf <3328151400@qq.com>
2023-11-13 17:31:32 +0800

    🦄 refactor(servo_control.c): 没加盖子的角度

--- EECM_programme/HARDWARE/servo/servo_control.c
+++ EECM_programme/HARDWARE/servo/servo_control.c
@@ -7,7 +7,7 @@
 #include "servo_control.h"
 #include "servo_apply.h"
 
-float   g_Vertical_Kp=1, g_Vertical_Ki=0.005, g_Vertical_D =-2; 
+float   g_Vertical_Kp=1.2, g_Vertical_Ki=0.006, g_Vertical_D =-2;
 int16_t g_Vertical_PWM;
 jy901s_angleData g_angleData;           //结构体名字
 
@@ -47,7 +47,7 @@ void Servo_Limit(void)
 void Servo_accomplish(void)
 {
 	    jy901s_getData(&g_angleData);
-        g_Vertical_PWM = Vertical(g_angleData.roll,0)*2.973 + 429;
+               g_Vertical_PWM = Vertical(g_angleData.pitch,0)*2.773 + 435;
 	    Servo_Limit();
 		Servo_SetAngle_Vertical(g_Vertical_PWM); 
 
